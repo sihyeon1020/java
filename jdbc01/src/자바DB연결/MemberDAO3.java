9b777f3d7a53603f68ffb7032a03efdc5ece71f0
이시현 <124111013+sihyeon1020@users.noreply.github.com>
2023-03-08 23:43:03 +0900

    Add files via upload

---
+++ jdbc01/src/자바DB연결/MemberDAO3.java
@@ -0,0 +1,139 @@
+package 자바DB연결;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import 화면DB연결.MemberVO;
+
+public class MemberDAO3 {
+
+	public int delete(MemberVO bag) {
+		
+		int result = 0; //결과값 저장하는 변수 만들어주기
+		
+		try {
+			// 1.오라클 11g와 연결한 부품 설정
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("1. 오라클과 자바 연결할 부품 설정 성공.");
+
+			// 2.오라클 11g에 연결해보자.(java --- oracle)
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String user = "system";
+			String password = "oracle";
+			Connection con = DriverManager.getConnection(url, user, password); // Connection
+			// String data = JOptionPane.showInputDialog("이름입력"); //String, 임아무개
+			System.out.println("2. 오라클 연결 성공.");
+
+
+			String sql = "delete from hr.MEMBER where id = ? ";
+			PreparedStatement ps = con.prepareStatement(sql); // PreparedStatement
+			ps.setString(1, bag.getId());
+			// con부품으로 sql스트링에 있는 것 SQL부품으로 만들어주세요.
+			System.out.println("3. SQL문 부품(객체)으로 만들어주기 성공.");
+
+			result = ps.executeUpdate(); // insert, update, delete문만!! sql문 실행결과가 int
+			System.out.println("4. SQL문 오라클로 보내기 성공.");
+			if(result == 1) { //조건에 따라서는 여러개가 될 수 있어서 >= 도 생각 
+				System.out.println("탈퇴 성공.");
+			}
+
+			// System.out.println(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		 return result;
+	}
+
+	public int update(MemberVO bag) {
+		int result = 0;
+		try {
+			// 1.오라클 11g와 연결한 부품 설정
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("1. 오라클과 자바 연결할 부품 설정 성공.");
+
+			// 2.오라클 11g에 연결해보자.(java --- oracle)
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String user = "system";
+			String password = "oracle";
+			Connection con = DriverManager.getConnection(url, user, password); // Connection
+			// String data = JOptionPane.showInputDialog("이름입력"); //String, 임아무개
+			System.out.println("2. 오라클 연결 성공.");
+
+
+			String sql = "update hr.MEMBER set tel = ? where id = ? ";
+			PreparedStatement ps = con.prepareStatement(sql); // PreparedStatement
+			ps.setString(1, bag.getTel());
+			ps.setString(2, bag.getId());
+
+			System.out.println("3. SQL문 부품(객체)으로 만들어주기 성공.");
+
+			result = ps.executeUpdate(); // insert, update, delete문만!! sql문 실행결과가 int
+			System.out.println("4. SQL문 오라클로 보내기 성공.");
+			
+			if (result == 1) {
+				System.out.println("수정 성공!");
+			}
+
+			// System.out.println(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+
+	// public void add2() {
+	public int insert(MemberVO bag) {
+
+		int result = 0;
+		
+		try {
+			// 1.오라클 11g와 연결한 부품 설정
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("1. 오라클과 자바 연결할 부품 설정 성공.");
+
+			// 2.오라클 11g에 연결해보자.(java --- oracle)
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String user = "system";
+			String password = "oracle";
+			Connection con = DriverManager.getConnection(url, user, password); // Connection
+			// String data = JOptionPane.showInputDialog("이름입력"); //String, 임아무개
+			System.out.println("2. 오라클 연결 성공.");
+
+		
+			// public void insert(String id, String pw, String name, String tel)
+			String sql = "insert into hr.MEMBER values (?, ?, ?, ?)";
+			PreparedStatement ps = con.prepareStatement(sql); // PreparedStatement
+			// con부품으로 sql스트링에 있는 것 SQL부품으로 만들어주세요.
+			// R빼고, 인덱스 0부터 시작!!
+			// 유일하게 db는 인덱스가 1부터 시작!!
+			// 2. 가방에서 값들을 하나씩 꺼내쓰세요.
+			ps.setString(1, bag.getId());
+			ps.setString(2, bag.getPw());
+			ps.setString(3, bag.getName());
+			ps.setString(4, bag.getTel());
+
+			System.out.println("3. SQL문 부품(객체)으로 만들어주기 성공.");
+
+			result = ps.executeUpdate(); // 1
+			System.out.println("4. SQL문 오라클로 보내기 성공.");
+			if (result == 1) {
+				System.out.println("회원가입 성공!");
+			}
+
+			// System.out.println(result);
+		} catch (Exception e) {
+			//insert가 제대로 안된 경우, 위험한 상황이라 판단
+			//catch가 실행
+			//실행된 Row수가 없으므로 result에 0을 넣어주자!
+			//result = 0;
+		
+			e.printStackTrace();
+		}
+		System.out.println(result);
+		return result;
+	}
+
+}
